6f98bda15496cf025e8eea33c91ec9d5a1668ec4
Thomas de Jong <abczzz13@gmail.com>
2022-01-16 18:29:10 +0100

    Revert "Finished pset7/movies exercise"
    This reverts commit c2c36524f70049f8d5c01cd72becb2bcbcacde43.

--- pset7/movies/10.sql
+++
@@ -1,4 +0,0 @@
-SELECT DISTINCT(people.name)
-FROM people
-JOIN directors ON people.id = directors.person_id
-WHERE directors.movie_id IN (SELECT movie_id FROM ratings WHERE rating >= 9.0);
--- pset7/movies/13.sql
+++
@@ -1,8 +0,0 @@
-SELECT DISTINCT(people.name)
-FROM people
-JOIN stars ON people.id = stars.person_id
-WHERE stars.movie_id IN (
-SELECT stars.movie_id
-FROM stars
-JOIN people ON people.id = stars.person_id
-WHERE people.name = 'Kevin Bacon' AND people.birth = 1958) AND people.name != 'Kevin Bacon';
