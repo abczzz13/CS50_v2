86fbb05fd08605b995ccdfc9f9ea829bc400a661
Thomas de Jong <abczzz13@gmail.com>
2022-01-20 18:35:34 +0100

    Almost done with lab8, not sure about timing of the javascript effect

--- trivia/script-0.0.1.js
+++ trivia/script-0.0.1.js
@@ -1,6 +1,8 @@
 // Question 1: The Dirty Code
-const yes = document.querySelector('#yes');
+/*
 const test = document.createElement('p');
+
+const yes = document.querySelector('#yes');
 document.querySelector('#yes').addEventListener('click', function () {
     yes.style.backgroundColor = 'red';
     test.textContent = 'Incorrect!';
@@ -20,26 +22,56 @@ document.querySelector('#what').addEventListener('click', function () {
     what.style.backgroundColor = 'red';
     test.textContent = 'Incorrect!';
 });
+*/
+
 // Question 1: The Better Way
+/*
+const answerQ1 = document.querySelector('.q1');
+const q1Text = document.querySelector('#q1Text');
+
+answerQ1.addEventListener('click', function () {
+    if (answerQ1.value === 'There is no question') {
+        answerQ1.style.backgroundColor = 'green';
+        q1Text.textContent = 'Correct!';
+    } else {
+        answerQ1.style.backgroundColor = 'red';
+        q1Text.textContent = 'Incorrect!';
+    }
+});
+*/
+
+const buttons = document.querySelectorAll('.q1')
+const q1Text = document.querySelector('#q1Text');
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    if(btn.value === 'question') {
+        btn.style.backgroundColor = 'green';
+        q1Text.textContent = 'Correct!';
+    } else {
+        btn.style.backgroundColor = 'red';
+        q1Text.textContent = 'Incorrect!';
+    }
+  });
+});
 
 
 // Question 2: The Dirty Code
 
 // Declaring Variables
-const answer = document.querySelector('#q2.q2').value;
+const answer = document.querySelector('.q2');
 const field = document.querySelector('#q2');
 const submit = document.querySelector('.submit');
+const q2Text = document.querySelector('#q2Text');
 
 // Checking after submitting
 document.querySelector('.submit').addEventListener('click', function () {
-    const para = document.createElement('p');
-    if (answer === '...') {
+    if (answer.value === 'There is no question') {
         field.style.backgroundColor = 'green';
-        para.textContent = 'Correct!';
-    }
-    else {
+        q1Text.textContent = 'Correct!';
+    } else {
         field.style.backgroundColor = 'red';
-        para.textContent = 'Incorrect!';
+        q2Text.textContent = 'Incorrect!';
     }
     document.body.appendChild(para);
 });
